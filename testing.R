47e0f2777a38aee5c2a41941a05a9a65ab34247a
cord phelps <rc.phelps@gmail.com>
2018-07-10 22:03:15 -0700

    7-10

--- testing.R
+++ testing.R
@@ -1,28 +1,40 @@
 
-# for each day, calculate the number of species observed
-bugs.df %>% 
-  dplyr::filter(transect=="oakMargin") %>%
-  dplyr::group_by(julian) %>% 
-  dplyr::summarise(totalBugs=ifelse(Thomisidae..crab.spider. > 0, 1, 0))
 
-
-tribble(
-  ~x, ~y, ~z, ~w, ~v, ~u,
-  "a", 175, 0, 1, 0, 0,
-  "b", 176, 1, 1, 1, 1,
-  "c", 175, 1, 0, 1, 0
-) -> test.tbl
-
-test.tbl %>% 
-  dplyr::filter(y>170) %>%
-  dplyr::group_by(y) %>% 
-  dplyr::summarise(totalBugs=sum(z, w, v, u))
-
-test.tbl %>% 
-  mutate(newColumn = ifelse(w > 0, 1, 0))
-
-
-test.tbl %>% 
-  +     mutate(newColumn = ifelse(w > 0, 1, 0))
-cbind(test.tbl, zeros=t(t(rowSums(test.tbl==0))))
-cbind(new.tbl, nonZeros=4-new.tbl$zeros)
+diversityZeros <- function(data.tbl, whichColumns) {
+  
+  library(tidyverse)
+  library(rlang)
+  
+  # for each row in a table, count zeros and non-zeros
+  # across specified columns; add two new columns
+  
+  # arguments
+  # data.tble : tribble
+  # countZero : boolean
+  # whichColumns : an expression of 2 integers separated by colon and packaged by rlang::quos() 
+  #                example :  quos(2:4)
+  
+  # return : new table with 2 new columns of counts
+  
+  # ref : https://stackoverflow.com/questions/37731987/count-number-of-values-in-row-using-dplyr
+  #       https://stackoverflow.com/questions/44593596/how-to-pass-strings-denoting-expressions-to-dplyr-0-7-verbs
+  #       https://community.rstudio.com/t/should-tidyeval-be-abandoned/2238/55
+  # testing data :
+  if (FALSE) {
+    tribble(
+      ~x, ~y, ~z, ~w, ~v, ~u,
+      "a", 175, 0, 1, 0, 0,
+      "b", 176, 1, 1, 1, 1,
+      "c", 175, 1, 0, 1, 0
+    ) -> test.tbl
+  }
+    
+  test.tbl <- data.tbl %>% select(UQS(whichColumns))
+  
+  temp1.tbl <- cbind(diversityZeros=t(t(rowSums(test.tbl==0))), diversityNonZeros=t(t(rowSums(data.tbl!=0)))  )
+  
+  temp1.tbl 
+  
+  return(dplyr::mutate(data.tbl, temp1.tbl ))
+  
+}
