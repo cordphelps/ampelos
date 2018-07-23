ec0d6c149b0fef7a3e3c0726f934003e02843960
cord phelps <rc.phelps@gmail.com>
2018-07-22 20:25:27 -0700

    203

--- code/QA.R
+++ code/QA.R
@@ -1,16 +1,24 @@
 # https://www.rstudio.com/wp-content/uploads/2015/02/rmarkdown-cheatsheet.pdf
 # https://tex.stackexchange.com/questions/152488/suppress-library-comments-from-output-with-knitr
 
-############### add to README ##########################
-# [![transect layout](https://raw.githubusercontent.com/cordphelps/ampelos/master/images/transectLayout.jpg)]()
-################ add to README #######################
+
 
 ##################### QA #############################
+
+
+# get all julian
+bugs.df %>% select(julian) %>%
+   				unique() %>% 
+   				.$julian
+
 # QA : bugs.df %>% dplyr::group_by(julian) %>% skim() 
-# bugs.df %>% dplyr::filter(julian==158) %>% skim() 
+bugs.df %>% dplyr::filter(julian==158) %>% skim() 
+
+
+
 # https://stackoverflow.com/questions/22353633/filter-for-complete-cases-in-data-frame-using-dplyr-case-wise-deletion
-# bugs.df %>% dplyr::filter(complete.cases(.))
-# bugs.df %>% dplyr::filter(!complete.cases(.))
+bugs.df %>% dplyr::filter(complete.cases(.))
+bugs.df %>% dplyr::filter(!complete.cases(.))
 ######################################################
 
 
