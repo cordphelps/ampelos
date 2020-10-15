5e0dd4c640f69eea0efeef45b0a2970fbbea1cc4
cord phelps <rc.phelps@gmail.com>
2020-10-14 22:20:33 -0700

    V2.2

--- code/ampelosRoundTwo.md
+++ code/ampelosRoundTwo.md
@@ -1,209 +1,31 @@
 ampelos V2
 ================
 
-``` r
-library(tidyr)
-library(dplyr)
-```
-
-    ## 
-    ## Attaching package: 'dplyr'
-
-    ## The following objects are masked from 'package:stats':
-    ## 
-    ##     filter, lag
-
-    ## The following objects are masked from 'package:base':
-    ## 
-    ##     intersect, setdiff, setequal, union
-
-``` r
-library(ggplot2)
-
-# "weeks 23-25",  "weeks 26-31", "weeks 32-34"
-
-source.url <- c("https://raw.githubusercontent.com/cordphelps/ampelos/master/data/bugs.csv")
-
-
-bugs.tibl <- as_tibble(read.csv(source.url, header=TRUE, row.names=NULL))
-
-
-thomisidae.period1.oakMargin.tibl <- bugs.tibl %>% 
-  filter(time != 'am') %>%
-  filter(transect == 'oakMargin') %>%
-  filter(week < 26) %>%
-  group_by(position) %>%
-    summarize(mean = mean(Thomisidae..crab.spider., na.rm=TRUE))
-```
-
+    ## `summarise()` ungrouping output (override with `.groups` argument)
+    ## `summarise()` ungrouping output (override with `.groups` argument)
+    ## `summarise()` ungrouping output (override with `.groups` argument)
+    ## `summarise()` ungrouping output (override with `.groups` argument)
+    ## `summarise()` ungrouping output (override with `.groups` argument)
     ## `summarise()` ungrouping output (override with `.groups` argument)
 
-``` r
-thomisidae.period1.control.tibl <- bugs.tibl %>% 
-  filter(time != 'am') %>%
-  filter(transect == 'control') %>%
-  filter(week < 26) %>%
-  group_by(position) %>%
-    summarize(mean = mean(Thomisidae..crab.spider., na.rm=TRUE))
-```
+<img src="ampelosRoundTwo_files/figure-gfm/bugs-1.png" width="50%" /><img src="ampelosRoundTwo_files/figure-gfm/bugs-2.png" width="50%" />
 
-    ## `summarise()` ungrouping output (override with `.groups` argument)
-
-``` r
-thomisidae.period2.oakMargin.tibl <- bugs.tibl %>% 
-  filter(time != 'am') %>%
-  filter(transect == 'oakMargin') %>%
-  filter(week > 25 & week < 32) %>%
-  group_by(position) %>%
-    summarize(mean = mean(Thomisidae..crab.spider., na.rm=TRUE))
-```
-
-    ## `summarise()` ungrouping output (override with `.groups` argument)
-
-``` r
-thomisidae.period2.control.tibl <- bugs.tibl %>% 
-  filter(time != 'am') %>%
-  filter(transect == 'control') %>%
-  filter(week > 25 & week < 32) %>%
-  group_by(position) %>%
-    summarize(mean = mean(Thomisidae..crab.spider., na.rm=TRUE))
-```
-
-    ## `summarise()` ungrouping output (override with `.groups` argument)
-
-``` r
-thomisidae.period3.oakMargin.tibl <- bugs.tibl %>% 
-  filter(time != 'am') %>%
-  filter(transect == 'oakMargin') %>%
-  filter(week > 31) %>%
-  group_by(position) %>%
-    summarize(mean = mean(Thomisidae..crab.spider., na.rm=TRUE))
-```
-
-    ## `summarise()` ungrouping output (override with `.groups` argument)
-
-``` r
-thomisidae.period3.control.tibl <- bugs.tibl %>% 
-  filter(time != 'am') %>%
-  filter(transect == 'control') %>%
-  filter(week > 31) %>%
-  group_by(position) %>%
-    summarize(mean = mean(Thomisidae..crab.spider., na.rm=TRUE))
-```
-
-    ## `summarise()` ungrouping output (override with `.groups` argument)
-
-``` r
-combo.period1.tibl <- bugs.tibl %>% 
-  filter(time != 'am') %>%
-  filter(week < 26)
-
-combo.period2.tibl <- bugs.tibl %>% 
-  filter(time != 'am') %>%
-  filter(week > 25 & week < 32)
-
-combo.period3.tibl <- bugs.tibl %>% 
-  filter(time != 'am') %>%
-  filter(week > 31)
-
-
-library(JGmisc)
-```
-
-    ## Loading required package: magrittr
-
-    ## 
-    ## Attaching package: 'magrittr'
-
-    ## The following object is masked from 'package:tidyr':
-    ## 
-    ##     extract
-
-``` r
-JGmisc::cohens.d(thomisidae.period1.oakMargin.tibl$mean, thomisidae.period1.control.tibl$mean)
-```
-
-    ## 
-    ##  Cohen's d measure of effect size
-    ## 
-    ## d: 0.757
-    ## data: thomisidae.period1.oakMargin.tibl$mean thomisidae.period1.control.tibl$mean
-    ## m1 = 0.45    m2 = 0.57
-    ## sd1 = 0.14   sd2 = 0.16
-    ## pooled variance: 0.023
-
-``` r
-JGmisc::cohens.d(thomisidae.period2.oakMargin.tibl$mean, thomisidae.period2.control.tibl$mean)
-```
-
-    ## 
-    ##  Cohen's d measure of effect size
-    ## 
-    ## d: 0.0631
-    ## data: thomisidae.period2.oakMargin.tibl$mean thomisidae.period2.control.tibl$mean
-    ## m1 = 0.14    m2 = 0.13
-    ## sd1 = 0.06   sd2 = 0.07
-    ## pooled variance: 0.004
-
-``` r
-JGmisc::cohens.d(thomisidae.period3.oakMargin.tibl$mean, thomisidae.period3.control.tibl$mean)
-```
-
-    ## 
-    ##  Cohen's d measure of effect size
-    ## 
-    ## d: 0.085
-    ## data: thomisidae.period3.oakMargin.tibl$mean thomisidae.period3.control.tibl$mean
-    ## m1 = 0.06    m2 = 0.05
-    ## sd1 = 0.06   sd2 = 0.07
-    ## pooled variance: 0.004
-
-``` r
-# effect size, early season: d = 0.757   ('large' Cohen 1988)
-# effect size, mid season:   d = 0.0348  ('very small' Sawilowsky, 2009)
-# effect size, late season:  d = 0.299   ('small-medium' Cohen 1988)
-#
-#
-# Altieri figure 4
-# approx effect size, early season: (negative)
-# approx effect size, mid season:   approx .5 'medium'
-# approx effect size, late season:  approx .2 'small'
-
-pwr::pwr.2p.test(h=0.757, sig.level=0.05, power=0.8)
-```
-
-    ## 
-    ##      Difference of proportion power calculation for binomial distribution (arcsine transformation) 
-    ## 
-    ##               h = 0.757
-    ##               n = 27.39333
-    ##       sig.level = 0.05
-    ##           power = 0.8
-    ##     alternative = two.sided
-    ## 
-    ## NOTE: same sample sizes
+<img src="ampelosRoundTwo_files/figure-gfm/rawDistributions-1.png" width="50%" /><img src="ampelosRoundTwo_files/figure-gfm/rawDistributions-2.png" width="50%" />
 
 <img src="ampelosRoundTwo_files/figure-gfm/densityPlots-1.png" width="50%" /><img src="ampelosRoundTwo_files/figure-gfm/densityPlots-2.png" width="50%" /><img src="ampelosRoundTwo_files/figure-gfm/densityPlots-3.png" width="50%" /><img src="ampelosRoundTwo_files/figure-gfm/densityPlots-4.png" width="50%" /><img src="ampelosRoundTwo_files/figure-gfm/densityPlots-5.png" width="50%" /><img src="ampelosRoundTwo_files/figure-gfm/densityPlots-6.png" width="50%" />
 
-    ## Warning in wilcox.test.default(x = c(0.37037037037037, 0.444444444444444, :
-    ## cannot compute exact p-value with ties
-
-    ## Warning in wilcox.test.default(x = c(0.0833333333333333, 0.0625, 0.125, : cannot
-    ## compute exact p-value with ties
-
-    ## Warning in wilcox.test.default(x = c(0, 0.0555555555555556,
-    ## 0.0555555555555556, : cannot compute exact p-value with ties
+| period  | cohens.d | sig.level | power | impliedSampleSize | actualSampleSize |
+| :------ | -------: | --------: | ----: | ----------------: | ---------------: |
+| period1 |     0.76 |      0.05 |   0.8 |                27 |              540 |
+| period2 |     0.06 |      0.05 |   0.8 |              3942 |              960 |
+| period3 |     0.09 |      0.05 |   0.8 |              2172 |              360 |
 
 <img src="ampelosRoundTwo_files/figure-gfm/errorBars-1.png" width="33%" /><img src="ampelosRoundTwo_files/figure-gfm/errorBars-2.png" width="33%" /><img src="ampelosRoundTwo_files/figure-gfm/errorBars-3.png" width="33%" />
 
     ## Warning: Ignoring unknown parameters: width
 
-    ## Warning in rm(label.lst): object 'label.lst' not found
-
-<img src="ampelosRoundTwo_files/figure-gfm/bayes-1.png" width="50%" />
+<img src="ampelosRoundTwo_files/figure-gfm/bayesPrimaryPlots-1.png" width="50%" /><img src="ampelosRoundTwo_files/figure-gfm/bayesPrimaryPlots-2.png" width="50%" />
 
 <img src="ampelosRoundTwo_files/figure-gfm/posteriorGraphs-1.png" width="33%" /><img src="ampelosRoundTwo_files/figure-gfm/posteriorGraphs-2.png" width="33%" /><img src="ampelosRoundTwo_files/figure-gfm/posteriorGraphs-3.png" width="33%" />
-
-<img src="ampelosRoundTwo_files/figure-gfm/printDiags1-1.png" width="50%" />
 
 <img src="ampelosRoundTwo_files/figure-gfm/printDiags2-1.png" width="33%" /><img src="ampelosRoundTwo_files/figure-gfm/printDiags2-2.png" width="33%" /><img src="ampelosRoundTwo_files/figure-gfm/printDiags2-3.png" width="33%" /><img src="ampelosRoundTwo_files/figure-gfm/printDiags2-4.png" width="33%" /><img src="ampelosRoundTwo_files/figure-gfm/printDiags2-5.png" width="33%" /><img src="ampelosRoundTwo_files/figure-gfm/printDiags2-6.png" width="33%" /><img src="ampelosRoundTwo_files/figure-gfm/printDiags2-7.png" width="33%" /><img src="ampelosRoundTwo_files/figure-gfm/printDiags2-8.png" width="33%" /><img src="ampelosRoundTwo_files/figure-gfm/printDiags2-9.png" width="33%" />
--- code/bayesNoClusters.R
+++ code/bayesNoClusters.R
@@ -282,7 +282,7 @@ generateLikelihoodV3 <- function(df, daytime, fromDisc, path, randomSeed,  hp) {
 
 
     ggList <- list()
-    # plot the likelihood for the 9 models
+    # plot the likelihood for the 3 models
     ggList[[1]] <- plotLikelihoodV2(tibble=as_tibble(likelihood.df),  hypoPop=hp,
                                   	cap=paste("(daylight hours)", sep=""))
 
@@ -316,7 +316,7 @@ generateLikelihoodV3 <- function(df, daytime, fromDisc, path, randomSeed,  hp) {
         }  
 
       # plot the posterior distributions
-      ggList[[i+1]] <- post.df.list[[i]] %>%
+      ggList[[i+2]] <- post.df.list[[i]] %>%
 
       select(-lp__) %>% 
       rename(b_interaction = `b_log_pop:contact_high`) %>%
@@ -356,7 +356,7 @@ generateLikelihoodV3 <- function(df, daytime, fromDisc, path, randomSeed,  hp) {
     rm(likelihood.df)
     rm(modelInput)
     rm(modelOutput)
-    rm(label.lst)
+    rm(weekLabel.lst)
     rm(post.df.list)
 
     #detach("package:rstan", unload=TRUE)  
@@ -396,7 +396,7 @@ plotLikelihoodV2 <- function(tibble, hypoPop, cap) {
     	scale_x_discrete(labels=c("one" = "weeks\n23-25", "two" = "weeks\n26-31", "three" = "weeks\n32-34"),
     					limits=c("one", "two", "three")) +
     
-    	coord_fixed(1.5) + # control the aspect ratio of the output
+    	# coord_fixed(1.5) + # control the aspect ratio of the output
     
     	theme_bw() +
 
@@ -775,4 +775,406 @@ modelMCMCcheckV2 <- function(models, daytime, debug) {
   return(gg.lst)
 
 }
+
+
+
+plotRawWeeklyV2 <- function(day, night, where) {
+
+
+
+  # plot daily spider counts, by week, differentiate by time
+  
+  # input 2 dataframes :
+  #  week, transect, time{am, pm}, totalSpiders
+  #
+
+  # > head(total.df, 10)
+  #   week  transect time totalSpiders
+  #1    23 oakMargin   am             2
+  #2    23 oakMargin   am             6
+  #3    23 oakMargin   am             2
+  #4    23 oakMargin   am             4
+  #5    23 oakMargin   am             2
+  #6    23 oakMargin   am             3
+  #7    23 oakMargin   pm             1
+  #8    23 oakMargin   pm             7
+  #9    23 oakMargin   pm            14
+  #10   23 oakMargin   pm             5
+  
+  #assign("plotWeekly.df", df, envir=.GlobalEnv)
+  #assign("dfX", df, envir=.GlobalEnv)
+  
+  colours = c("pm" = "violet", "am" = "purple")
+  
+  gg <- ggplot() + 
+    
+    geom_jitter(data=day, aes(x=week, y=totalSpiders, fill=time), size=3, shape=21, alpha=.7, show.legend=TRUE, width=.2, height=.2) +
+    geom_jitter(data=night, aes(x=week, y=totalSpiders, fill=time), size=3, shape=21, alpha=.7, show.legend=TRUE, width=.2, height=.2) +
+    
+    geom_vline(xintercept=25.5) + # seasonal timeframe seperators
+    geom_vline(xintercept=31.5) + #
+    
+    ylim(c(0, 31)) + 
+    expand_limits(y=c(0,31)) + 
+    coord_fixed(ratio=1/4) +     # control the aspect ratio of the output; "ratio" refers to the 
+  # ratio of the axis limits themselves
+    
+    scale_y_continuous(breaks = seq(min(0), max(65), by = 5)) +
+    scale_x_continuous(breaks=seq(22,40,2)) + 
+    
+    # labs(title=paste("total spiders trapped by week", sep=""),
+    labs(
+         y="Thomisidae, daily counts", 
+         x="week", 
+         caption = paste(where, " transect population activity\nby collection time in three\nobserved seasonal periods", 
+         				 sep="") ) +
+    
+    theme_bw() +
+
+    scale_fill_manual(values = colours, 
+                      breaks = c("am", "pm"),
+                      labels = c("overnight", "daytime")) +
+    
+    theme(legend.title = element_blank(),
+          legend.spacing.y = unit(0, "mm"), 
+          #legend.position=c(.9,.7),
+          legend.justification=c(1,0),
+          panel.border = element_rect(colour = "black", fill=NA),
+          aspect.ratio = 1, axis.text = element_text(colour = 1, size = 12),
+          legend.background = element_blank(),
+          legend.box.background = element_rect(colour = "black")) 
+    
+  
+  
+  return(gg)
+  
+}
+
+getWeeks <- function(data) {
+
+	# return a list of the weeks occurring in the dataset
+	# https://stackoverflow.com/questions/29832411/use-dplyr-to-get-values-of-a-column
+
+	library(dplyr)
+
+	weeks <- data %>%
+   				select(week) %>%
+   				unique() %>% 
+   				.$week
+
+   	return(weeks)
+
+}
+
+scanBugPercentages <- function(df) {
+
+  # calculate count and percent by week (columns) for each bug (rows)
+
+  if (FALSE) {
+
+    df <- bugs.df
+
+  }
+
+  weeks.vector <- getWeeks(df)
+  bugList <- colnames(df[,5:22])
+
+  bugPct <- as.data.frame(df %>% select(5:22) %>% names())   # bug names as first column
+  # disapointing : https://stackoverflow.com/questions/42769650/how-to-dplyr-rename-a-column-by-column-index
+  # bugPct <- bugPct %>% rename_at( 1, ~"column1" ) %>% tibble::column_to_rownames('column1')
+  bugPct <- bugPct %>% rename_at( 1, ~"bugNames" )  # 
+
+  pctColNames <- list()
+
+  for (i in 1:length(weeks.vector)) {
+
+    trim.tbl <- as.data.frame(df %>% filter(week == weeks.vector[[i]]) %>% select(5:22) %>% colSums()) # append column representing totals 
+                                                                                                       # of each insect for week i
+    trim.tbl <- cbind(trim.tbl,round(prop.table(trim.tbl)*100,2))  # append column representing the percent of each insect for week i
+
+    colnames(trim.tbl) <- c(paste('week', weeks.vector[[i]], 'count', sep=""), paste('week', weeks.vector[[i]], 'pct', sep=""))
+    rownames(trim.tbl) <- NULL
+
+    pctColNames[[i]] <- paste('week', weeks.vector[[i]], 'pct', sep="")  # save the column names
+
+    bugPct <- dplyr::bind_cols(bugPct, trim.tbl)
+
+  }
+
+  returnList <- list()
+  returnList[[1]] <- bugPct        # count and percent by week (columns) for each bug (rows)
+  returnList[[2]] <- pctColNames   # week23pct, week24pct, ....
+  returnList[[3]] <- weeks.vector  # 23, 24, .....
+
+  return(returnList)
+
+}
+
+createFamilyPercentages <- function(list) {
+
+  # given the dataframe and a list of columns returned by scanBugPercentages, organize percentages 
+  # by individual families, return dataframes for each family of insects showing pct by week 
+
+  #        list[[1]]   # dataframe with all the data : count and percent by week (columns) for each bug (rows)
+  #        list[[2]]   # list of columns that contain the weekly percentages
+  #        list[[3]]   # vector of week numbers
+
+  dfBase <- list[[1]] %>% dplyr::select(bugNames)  # one column of bugNames
+
+  for (i in 1:length(list[[2]])) {    # get the percentage columns
+
+    temp.df <- list[[1]] %>% dplyr::select(list[[2]][[i]])
+
+    dfBase <- dplyr::bind_cols(dfBase, temp.df)
+
+  }
+
+  # dfBase is now columns of weekly insect pct and rows for each insect
+  # we are transforming the data into dataframes for 'families' of specific insects
+  # 
+  # each will be the sum of the percentages for each species in the family
+  #
+  # dfAraneae :      columns:   otherPct   spiderPct   avePct    week
+  #
+
+  dfCrab <- dfBase %>% dplyr::filter(bugNames == 'Thomisidae..crab.spider.')  # remove all insects except spiders
+  dfOther <- dfBase %>% dplyr::filter(bugNames == 'spider.other') 
+  dfAraneae <- dplyr::union(dfCrab, dfOther)  # one row for each spider; columns are percent for each week
+  dfAraneae <- squashFlip(df=dfAraneae, weekList=list[[3]], columnList=c('otherPct', 'spiderPct', 'week'))
+  dfAraneae$family <- 'Araneae'
+
+  dfDiptera <- dfBase %>% dplyr::filter(bugNames == 'Diptera..Agromyzidae..leafminer..') 
+  dfDiptera <- squashFlip(df=dfDiptera, weekList=list[[3]], columnList=c('dipteraPct', 'week'))
+  dfDiptera$family <- 'Diptera'
+
+  dfH1 <- dfBase %>% dplyr::filter(bugNames == 'Braconid.wasp')
+  dfH2 <- dfBase %>% dplyr::filter(bugNames == 'Halictus.sp....3.part..native.bee.')
+  dfH3 <- dfBase %>% dplyr::filter(bugNames == 'Agapostemon.sp....green..native.bee.')
+  dfH4 <- dfBase %>% dplyr::filter(bugNames == 'Osmia.sp...native.bee.')
+  dfH5 <- dfBase %>% dplyr::filter(bugNames == 'Honey.Bee')
+  dfH6 <- dfBase %>% dplyr::filter(bugNames == 'Bombus.californicus..bumble.')
+  dfHymenoptera <- dplyr::union(dfH1, dfH2)
+  dfHymenoptera <- dplyr::union(dfHymenoptera, dfH3)
+  dfHymenoptera <- dplyr::union(dfHymenoptera, dfH4)
+  dfHymenoptera <- dplyr::union(dfHymenoptera, dfH5)
+  dfHymenoptera <- dplyr::union(dfHymenoptera, dfH6)
+  dfHymenoptera <- squashFlip(df=dfHymenoptera, weekList=list[[3]], columnList=c('a', 'b', 'c', 'd', 'e', 'f', 'week'))
+  dfHymenoptera$family <- 'Hymenoptera'
+
+  dfHe1 <- dfBase %>% dplyr::filter(bugNames == 'Lygus.hesperus..western.tarnished.plant.bug.')
+  dfHe2 <- dfBase %>% dplyr::filter(bugNames == 'pentamonidae...stinkBug.')
+  dfHemiptera <- dplyr::union(dfHe1, dfHe2)
+  dfHemiptera <- squashFlip(df=dfHemiptera, weekList=list[[3]], columnList=c('a', 'b', 'week'))
+  dfHemiptera$family <- 'Hemiptera'
+
+  dfLep1 <- dfBase %>% dplyr::filter(bugNames == 'checkerspot.butterfly')
+  dfLep2 <- dfBase %>% dplyr::filter(bugNames == 'Pyralidae..Snout.Moth.') 
+  dfLepidoptera <- dplyr::union(dfLep1, dfLep2)
+  dfLepidoptera <- squashFlip(df=dfLepidoptera, weekList=list[[3]], columnList=c('a', 'b', 'week'))
+  dfLepidoptera$family <- 'Lepidoptera'
+
+  dfOther1 <- dfBase %>% dplyr::filter(bugNames == 'Orius..pirate.bug.')
+  dfOther2 <- dfBase %>% dplyr::filter(bugNames == 'Diabrotica.undecimpunctata..Cucumber.Beetle.')
+  dfOther3 <- dfBase %>% dplyr::filter(bugNames == 'other')
+  dfOther4 <- dfBase %>% dplyr::filter(bugNames == 'ladyBug')
+  dfOther5 <- dfBase %>% dplyr::filter(bugNames == 'pencilBug')
+  dfOther <- dplyr::union(dfOther1, dfOther2)
+  dfOther <- dplyr::union(dfOther, dfOther3)
+  dfOther <- dplyr::union(dfOther, dfOther4)
+  dfOther <- dplyr::union(dfOther, dfOther5)
+  dfOther <- squashFlip(df=dfOther, weekList=list[[3]], columnList=c('a', 'b', 'c', 'd', 'e', 'week'))
+  dfOther$family <- 'Other'
+
+
+  if (FALSE) {  # a manual verification that the percentages sum to 1
+
+    dfAraneae.temp <- dfAraneae %>% dplyr::select(sumPct)       # that's a dataframe every column except week
+    dfDiptera.temp <- dfDiptera %>% dplyr::select(sumPct)
+    dfHymenoptera.temp <- dfHymenoptera %>% dplyr::select(sumPct) 
+    dfHemiptera.temp <- dfHemiptera %>% dplyr::select(sumPct) 
+    dfLepidoptera.temp <- dfLepidoptera %>% dplyr::select(sumPct) 
+    dfOther.temp <- dfOther %>% dplyr::select(sumPct) 
+
+    dfTotal <- dplyr::bind_cols(dfDiptera.temp, dfHymenoptera.temp)
+    dfTotal <- dplyr::bind_cols(dfTotal, dfHemiptera.temp)
+    dfTotal <- dplyr::bind_cols(dfTotal, dfLepidoptera.temp)
+    dfTotal <- dplyr::bind_cols(dfTotal, dfOther.temp)
+    dfTotal <- dplyr::bind_cols(dfTotal, dfAraneae.temp)
+
+
+    dfTotal <- transform(dfTotal, sum = rowSums(dfTotal, na.rm = TRUE))
+
+
+  }
+
+  returnList <- list()
+  returnList[[1]] <- dfAraneae
+  returnList[[2]] <- dfDiptera
+  returnList[[3]] <- dfHymenoptera
+  returnList[[4]] <- dfHemiptera
+  returnList[[5]] <- dfLepidoptera
+  returnList[[6]] <- dfOther
+
+  return(returnList)
+
+}
+
+
+plotBugPercentages <- function(list, spidersOnly) {
+
+  dfAraneae <- list[[1]]
+  dfDiptera <- list[[2]]
+  dfHymenoptera <- list[[3]]
+  dfHemiptera <- list[[4]]
+  dfLepidoptera <- list[[5]]
+  dfOther <- list[[6]]
+
+  if (spidersOnly==TRUE) {
+
+    gg <- ggplot(dfAraneae) + 
+    
+      geom_point(aes(x=week, y=spiderPct, fill = "spiderPct"), shape=21, size=5, show.legend=TRUE) +
+      geom_point(aes(x=week, y=otherPct, fill = 'otherPct'), shape=21, size=5, show.legend=TRUE) +
+
+      scale_fill_manual(name = "", values = c("spiderPct" = "purple", "otherPct" = "violet"), labels = c("Thomisidae", "Araneae (other)")) +
+
+      ylim(c(0, 30)) + 
+      expand_limits(y=c(0,30)) + 
+      #coord_fixed(ratio=1/4) +     # control the aspect ratio of the output; "ratio" refers to the 
+      # ratio of the axis limits themselves
+    
+      scale_y_continuous(breaks = seq(min(0), max(30), by = 5)) +
+      scale_x_continuous(breaks=seq(22,40,2)) + 
+    
+    
+      #labs(title=paste("spider abundance", sep=""),
+      #subtitle=paste("percent of total insects by week", sep=""), 
+      labs(y="spider percentage", 
+          x="week", 
+          caption = paste("spider abundance\n", "percent of total insects by week", sep="") ) +
+    
+      theme_bw() +
+    
+      theme(legend.title = element_blank(),
+          legend.spacing.y = unit(0, "mm"), 
+          #legend.position=c(.9,.7),
+          legend.justification=c(1,0),
+          panel.border = element_rect(colour = "black", fill=NA),
+          aspect.ratio = 1, axis.text = element_text(colour = 1, size = 12),
+          legend.background = element_blank(),
+          legend.box.background = element_rect(colour = "black")) 
+
+    } else {
+
+      colours = c("oakMargin" = "#405E00", "control" = "#9BCC94")
+
+      gg <- ggplot() + 
+    
+        geom_jitter(data=dfOther, aes(x=week, y=sumPct, fill = family), shape=21, size=5, show.legend=TRUE) +
+        geom_jitter(data=dfAraneae, aes(x=week, y=sumPct, fill = family), shape=21, size=5, show.legend=TRUE) +
+        geom_jitter(data=dfDiptera, aes(x=week, y=sumPct, fill = family), shape=21, size=5, show.legend=TRUE) +
+        geom_jitter(data=dfHymenoptera, aes(x=week, y=sumPct, fill = family), shape=21, size=5, show.legend=TRUE) +
+        geom_jitter(data=dfHemiptera, aes(x=week, y=sumPct, fill = family), shape=21, size=5, show.legend=TRUE) +
+        geom_jitter(data=dfLepidoptera, aes(x=week, y=sumPct, fill = family), shape=21, size=5, show.legend=TRUE) +
+
+        scale_fill_manual(name = 'insect family', 
+          values = c('black','green', 'blue', 'violet', 'purple', 'red'), 
+          breaks = c("Other", 'Araneae', 'Diptera', 'Hymenoptera', 'Hemiptera', 'Lepidoptera'),
+          labels = c("other", "Araneae", 'Diptera', 'Hymenoptera', 'Hemiptera', 'Lepidoptera')) +
+
+        #guides(shape = guide_legend("insect family"))  +
+
+        ylim(c(0, 100)) + 
+        expand_limits(y=c(0,100)) + 
+        #coord_fixed(ratio=1/4) +     # control the aspect ratio of the output; "ratio" refers to the 
+        # ratio of the axis limits themselves
+    
+        #scale_y_continuous(trans='log10', breaks = seq(min(0), max(100), by = 20)) +
+        scale_y_continuous(trans='log10') +
+        annotation_logticks(sides='l') +
+
+        scale_x_continuous(breaks=seq(22,40,2)) + 
+    
+    
+        #labs(title=paste("insect abundance by taxonometric Order", sep=""),
+        # subtitle=paste("percent of total population by week", sep=""),
+        labs(y="percent", x="week", 
+          caption = paste("insect abundance by taxonometric Order\n", "percent of total population by week", sep="") ) +
+    
+        theme_bw() +
+    
+        theme(legend.title = element_blank(),
+          legend.spacing.y = unit(0, "mm"), 
+          #legend.position=c(.9,.7),
+          legend.justification=c(1,0),
+          panel.border = element_rect(colour = "black", fill=NA),
+          aspect.ratio = 1, axis.text = element_text(colour = 1, size = 12),
+          legend.background = element_blank(),
+          legend.box.background = element_rect(colour = "black")) 
+
+      }
+    
+  return(gg)
+
+}
+
+
+squashFlip <- function(df, weekList, columnList) {
+
+  # convert row wise percent occurrance of bugs across weeks to 
+  # an average of these associated insects by week
+
+  # 
+  # inbound dataframe
+  #
+  #        bugNames   week1Pct   week2Pct  week3Pct  ...... 
+  # bugA
+  # bugB   
+  #
+  # weeklist   : a vector of week numbers occurring in the df
+  # columnList : a vector of row names ultimately to be applied as column names
+
+
+  if (FALSE) {
+    df <- dfShort
+    weekList <- list[[3]]
+    columnList <- c('otherPct', 'spiderPct', 'week')
+  }
+
+  df <- df %>% dplyr::select(-bugNames)  # remove the bugNames column
+  df <- df %>% rbind(as.character(weekList)) # add a row of week numbers
+  colnames(df) <- NULL
+  df <- as.data.frame(t(df)) # flip  = transpose https://stackoverflow.com/questions/6778908/transpose-a-data-frame
+  colnames(df) <- columnList
+  options(digits=4)  # https://stackoverflow.com/questions/26734913/r-converting-from-string-to-double
+  for (i in 1:length(columnList)) {
+    df[,columnList[[i]]] <- as.numeric(as.character((df[,columnList[[i]]]))) # https://stackoverflow.com/questions/3796266/change-the-class-from-factor-to-numeric-of-many-columns-in-a-data-frame
+  }
+
+  # now, just create an average of insects for this particular family dataframe. columnList represents the
+  # column names including 'week'. We could truncate the list to remove 'week', 
+  #          like so : columnList <- columnList [ - length(columnList) ] # ignore the week column (the last column in the list)
+  # and then use the modified columnList to define the columns that need to be used for the average
+  #           somewhat like : df <- df %>% rowwise() %>% dplyr::mutate(meanPct=mean(columnList))
+  # but mutate() is choking on that list, I think it is due to the old bogey of dyplr not accepting clean variables.
+  #            the solution may be here : https://datascience.blog.wzb.eu/2016/09/27/dynamic-columnvariable-names-with-dplyr-using-standard-evaluation-functions/
+  # the other approach is to remove the 'week' column, creat the average, then re-add the week column.
+  #
+  
+  temp.df <- df
+  temp.df <- temp.df %>% dplyr::select(week)                # that's a dataframe containing only week
+  df <- df %>% dplyr::select(-week)                         # that's a dataframe every column except week
+  df <- transform(df, sumPct = rowSums(df, na.rm = TRUE))  # finally, escaping the nightmare of dplyr https://stackoverflow.com/questions/12486264/average-across-columns-in-r-excluding-nas
+  df <- dplyr::bind_cols(df, temp.df)
+
+  # returning dataframe
+  #                                     /----------- use this average for graphing purposes
+  # species1.Pct .... speciesX.Pct   avePct   week
+  #
+  #
+
+  return(df)
+
+}
+
 
